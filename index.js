098c70e069534904201d6662d07049a4f4874825
H4R3N4 <arenanofidina@gmail.com>
2026-01-12 07:33:34 +0300

    read tasks variable on html

---
+++ index.js
@@ -0,0 +1,43 @@
+let tasks = [
+    {
+        id: 1,
+        design: 'Revision JS',
+        done: false
+    }
+];
+
+function renderTasks() {
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach(t=>{
+        const li = document.createElement("li");
+        li.textContent = t.design;
+        list.appendChild(li);
+    })
+}
+
+renderTasks();
+
+
+function addTask() {
+    const input = document.getElementById("inputNewTask");
+    const title = input.value;
+
+    const newTask = {
+        id: Date.now(),
+        design: title,
+        done: false
+    }
+
+    tasks.push(newTask);
+
+    console.log('tasks', tasks);
+
+    renderTasks();
+};
+
+document
+    .getElementById("btnAddNewTask")
+    .addEventListener("click",addTask);
+
