ac1db29b8df77b356347a346bcaaa2c6cb41c27e
H4R3N4 <arenanofidina@gmail.com>
2026-01-12 08:38:48 +0300

    Adding a line-through onClick on a task to mark it as done

--- index.js
+++ index.js
@@ -9,14 +9,35 @@ function loadTasks(){ //from localstorage
 
 loadTasks();
 
+function toggleTask(id){
+    const task = tasks.find(t=> t.id === id);
+
+    if (!task) return;
+
+    task.done = !task.done;
+
+    saveTasks();
+    renderTasks();
+}
+
 function renderTasks() {
     const list = document.getElementById("taskList");
     list.innerHTML = "";
 
     tasks.forEach(t=>{
         const li = document.createElement("li");
-        li.textContent = t.design;
-        list.appendChild(li);
+        li.textContent = t.design   ;
+        li.dataset.id = t.id;
+        
+        if(t.done){
+            li.style.textDecoration = "line-through";
+        }
+
+        li.addEventListener("click",() =>{
+            toggleTask(t.id);
+        });
+        
+        list.appendChild(li);       
     })
 }
 
@@ -28,7 +49,8 @@ renderTasks();
 
 function addTask() {
     const input = document.getElementById("inputNewTask");
-    const title = input.value;
+    const title = input.value.trim();
+    if (title === "") return;
     const newTask = {
         id: Date.now(),
         design: title,
